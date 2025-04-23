e33ebed94f256297e8f388a15a326470ebb277b5
ducable16 <ductrant1k29@gmail.com>
2025-04-23 22:38:58 +0700

    cors update

--- backend/src/main/java/com/config/CorsConfig.java
+++ backend/src/main/java/com/config/CorsConfig.java
@@ -15,6 +15,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("https://localhost:5173")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true); // nếu bạn dùng cookie hoặc Authorization. Vẫn JWT được bình thường nhé
