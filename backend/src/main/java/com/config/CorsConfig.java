a7125a4524dda01f786a5846808e0bc2aaa3a86f
ducable16 <ductrant1k29@gmail.com>
2025-04-24 22:31:15 +0700

    cors fixx

--- backend/src/main/java/com/config/CorsConfig.java
+++ backend/src/main/java/com/config/CorsConfig.java
@@ -14,10 +14,10 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "https://localhost:5173")
+                        .allowedOrigins("http://localhost:3000", "http://localhost:5173")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
-                        .allowCredentials(true); // nếu bạn dùng cookie hoặc Authorization. Vẫn JWT được bình thường nhé
+                        .allowCredentials(true);
             }
         };
     }
