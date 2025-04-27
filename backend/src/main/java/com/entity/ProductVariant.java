0f1050983e702c608fc7379fa7fb72a6419237be
ducable16 <ductrant1k29@gmail.com>
2025-04-27 13:53:41 +0700

    aaa

--- backend/src/main/java/com/entity/ProductVariant.java
+++ backend/src/main/java/com/entity/ProductVariant.java
@@ -1,5 +1,6 @@
 package com.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -23,6 +24,7 @@ public class ProductVariant {
 
     @ManyToOne
     @JoinColumn(name = "product_id", nullable = false)
+    @JsonIgnore
     private Product product;
 
     private String color;
