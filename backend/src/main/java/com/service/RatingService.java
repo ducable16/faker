103f2499133f6f989072ce592ea3c2f3e7e301f1
ducable16 <ductrant1k29@gmail.com>
2025-05-21 10:34:42 +0700

    a

--- backend/src/main/java/com/service/RatingService.java
+++ backend/src/main/java/com/service/RatingService.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RatingService {
@@ -40,6 +41,15 @@ public class RatingService {
     public Rating submitRating(String token, RatingRequest request) {
         String username = jwtService.extractUsername(token);
         User user = userRepository.findByUsername(username).get();
+        Optional<Rating> checkRating = ratingRepository.findByProductIdAndUserId(request.getProductId(), user.getUserId());
+        if(checkRating.isPresent()) {
+            Rating rating = checkRating.get();
+            rating.setRating(request.getRating());
+            rating.setRateComment(request.getRatingComment());
+            rating.setRatingDate(LocalDateTime.now());
+            ratingRepository.save(rating);
+            return rating;
+        }
         Rating rating = new Rating();
         rating.setUserId(user.getUserId());
         rating.setProductId(request.getProductId());
